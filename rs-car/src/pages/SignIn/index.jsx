132a500374285100c5623fad93b3488298074c7a
Rafael Souza <rafael93.souzasfc5@gmail.com>
2022-11-18 21:53:23 -0300

    Adicionado estilização a pagina de login

--- rs-car/src/pages/SignIn/index.jsx
+++ rs-car/src/pages/SignIn/index.jsx
@@ -1,9 +1,63 @@
+import { useState } from "react";
 import "./style.css";
+import logo from '../../assets/icons/logo-rs-car.svg';
+import iconShowPassword from '../../assets/icons/icon-password-show.svg';
+import iconHidePassword from '../../assets/icons/icon-password-hide.svg';
 
 function SignIn() {
+    const [form, setForm] = useState({ email: "", password: "" });
+    const [showPassword, setShowPassword] = useState(true);
+
     return (
-        <div>
-            <h1>Sign IN</h1>
+        <div className="container-sign-in flex-row">
+            <div className="div-form-sign-in flex-column-center">
+
+                <img className="logo-sign-in" src={logo} alt="logo RS Car" />
+                <form className="form-sign-in">
+                    <h1 className="form-h1">Faça login</h1>
+                    <div className="flex-column div-input-sign-in">
+                        <label
+                            className="label-sign-in"
+                            htmlFor="email">
+                            E-mail
+                        </label>
+                        <input
+                            className="input-sign-in"
+                            id="email"
+                            type="email"
+                            name="email"
+                            value={form.email}
+                            onChange={(e) => setForm({ ...form, [e.target.name]: e.target.value })}
+                        />
+                    </div>
+                    <div className="flex-column div-input-sign-in relative">
+                        <img
+                            onClick={() => setShowPassword(!showPassword)}
+                            className="icon-show-password"
+                            src={showPassword ? iconHidePassword : iconShowPassword}
+                            alt={showPassword ? "Clique para mostrar senha" : "clique para esconder senha"}
+                        />
+                        <label
+                            className="label-sign-in"
+                            htmlFor="password">
+                            Senha
+                        </label>
+                        <input
+                            className="input-sign-in"
+                            id="password"
+                            type={showPassword ? "password" : "text"}
+                            name="password"
+                            value={form.password}
+                            onChange={(e) => setForm({ ...form, [e.target.name]: e.target.value })}
+                        />
+                    </div>
+                    <div className="div-button-sign-in flex-row-center">
+                        <button className="btn-sign-in">Entrar</button>
+                    </div>
+                </form>
+            </div>
+
+
         </div>
     )
 }
